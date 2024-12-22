c5b71185874a630c10f1450bf478f8b5b7ee398b
BhuannaichFhirinn <bhuannaich.fhirinn@gmail.com>
2024-12-22 02:22:19 +0000

    Validation isUndefinedNullOrEmpty method added.

--- src/utiles/validation.js
+++ src/utiles/validation.js
@@ -1,6 +1,15 @@
 // Check if a value is null or undefined
 export const isNullOrUndefined = (value) => value === null || value === undefined;
 
+// Checks if a value is undefined, null, NaN, an empty string, array, or object.
+export const isUndefinedNullOrEmpty = (object) => {
+  if (object === undefined || object === null || Number.isNaN(object)) return true;
+  if (typeof object === 'string' && object.trim().length === 0) return true;
+  if (Array.isArray(object) && object.length === 0) return true;
+  if (typeof object === 'object' && object.constructor === Object && Object.keys(object).length === 0) return true;
+  return false;
+}
+
 // Check if an array is empty
 export const isEmptyArray = (array) => Array.isArray(array) && array.length === 0;
 
